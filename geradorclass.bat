00d21cbf0d54064ff6a67d1940b0f94ea0d38fe4
Murilo Fuscolin <murilo.fuscolin@fulltechpro.com.br>
2021-10-08 10:48:09 -0300

    enviando o bat

---
+++ geradorclass.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+CLS
+ECHO ***
+ECHO Runs xsd.exe on all *.xsd files sorted by filename in the current folder.
+ECHO ***
+
+FOR /R "C:\your\file" %%G IN (*.xsd) DO (
+  @ECHO ON
+	"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\xsd.exe" "%%G" /c /n:Mynamespace /o:"C:\your\file"
+  @ECHO OFF
+)
